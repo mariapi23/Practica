82b9762f0ee95a941e60a31cd136618f653ab33c
maria <maria.pi.23.55@gmail.com>
2017-10-27 15:31:50 +0200

    Entrar y registrarse con tecla enter.

--- WebContent/resources/js/index.js
+++ WebContent/resources/js/index.js
@@ -1,7 +1,9 @@
+var tab = 1;
 $( document ).ready(function() {
 	$(function() {
 
 	    $('#login-form-link').click(function(e) {
+	    	tab=1;
 	    	$("#login-form").delay(100).fadeIn(100);
 	 		$("#register-form").fadeOut(100);
 			$('#register-form-link').removeClass('active');
@@ -9,6 +11,7 @@ $( document ).ready(function() {
 			e.preventDefault();
 		});
 		$('#register-form-link').click(function(e) {
+			tab=2;
 			$("#register-form").delay(100).fadeIn(100);
 	 		$("#login-form").fadeOut(100);
 			$('#login-form-link').removeClass('active');
@@ -63,7 +66,6 @@ $( document ).ready(function() {
 	});
 	
 	$('#register-submit').click(function(){
-		
 		var email = $('#email-register').val();
 		var username = $('#username').val();
 		var password = $('#password-register').val();
@@ -103,9 +105,76 @@ $( document ).ready(function() {
 		});
 	})
 	
+	$(document).keypress(function (e){
+		if(e.which == 13){
+			
+			if (tab==1){
+				var email = $('#email').val();
+				var pwd = $('#password').val();
+				$.post( "login.do",{ email: email, password: pwd}, function( data ) {
+					var json = JSON.parse(data);
+					if(json.status=='ok'){
+						//GUARDAR EL NOMBRE Y EMAIL EN LOCALSTORAGE
+						//localStorage.setItem("name", json.name);
+						//localStorage.setItem("email", json.email);
+						//localStorage.setItem("status", json.status);
+						sessionStorage.setItem("name", json.name);
+						sessionStorage.setItem("email", json.email);
+						sessionStorage.setItem("status", json.status);
+						
+						// REDIRECCIÓN AL MURO.
+						location.href ="wall.jsp";
+					}else{
+						alert(json.message);
+					}
+				});
+			}else if(tab==2){
+				var email = $('#email-register').val();
+				var username = $('#username').val();
+				var password = $('#password-register').val();
+				var password2 = $('#confirm-password').val();
+				$.post( "consultar.do",{ email: email }, function( data ) {
+					 if(data=='ok'){
+						 
+						 if (email=='' || username==''){
+							 alert("No puedes dejar vacio el usuario o la contraseña");
+						 }else{
+							 $.post( "registro.do",
+									 { email: email, password: password , username:username, 'confirm-password': password2}, 
+									 
+								function( data ) {
+									var json = JSON.parse(data);
+									if(json.status=='ok'){
+										//GUARDAR EL NOMBRE Y EMAIL EN LOCALSTORAGE
+										//localStorage.setItem("name", json.name);
+										//localStorage.setItem("email", json.mail);
+										//localStorage.setItem("status", json.status);
+										sessionStorage.setItem("name", json.name);
+										sessionStorage.setItem("email", json.mail);
+										sessionStorage.setItem("status", json.status);
+										// REDIRECCIÓN AL MURO.
+										location.href ="wall.jsp";
+										
+									}else{
+										alert(json.message);
+									}
+								});
+						 }
+					 }else{
+						 $("#email-register").css('border-color', 'red');
+						 $("#error-email").css("color","red");
+						 $("#error-email").text("Correo electrónico en uso");
+					 }
+				});
+			}
+		
+		}
+	});
+	
 	$('#password').keyup(function(){
 		$('.error-list').hide();
-	})
+	});
+	
 	
 	$('#login-submit').click(function(){
 		var email = $('#email').val();
